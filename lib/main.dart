ce43220ff90bad72e41c3970309e105dcfb3c906
Yeon0023 <yeon0023@e.ntu.edu.sg>
2022-11-05 00:46:33 +0800

    051122

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ void main(context) async {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: const VerifyEmailView(),
+      home: const HomePage(),
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.teal[900],
         primaryColor: Colors.teal[900],
